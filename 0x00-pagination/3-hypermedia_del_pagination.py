2de9c9d78a81a89aef93cdf5b51cedd3a87b6614
evans-manyala <evans.manyala@gmail.com>
2024-07-21 19:35:13 +0300

    Adding more code logic

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,4 +40,31 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+        """
+        Retrieves a dictionary of paginated data,
+        gracefully handling deletions.
+        """
+
+        error = "Invalid index"
+        assert index is None or (index >= 0 and
+                                 index < len(self.__dataset)), error
+        index = index if index is not None else 0
+
+        indexed_dataset = self.indexed_dataset()
+        indexed_keys = sorted(indexed_dataset.keys())
+
+        data = []
+        pos_index = index
+        while len(data) < page_size and pos_index < len(indexed_dataset):
+            if pos_index in indexed_dataset:
+                data.append(indexed_dataset[pos_index])
+            pos_index += 1
+
+        next_index = pos_index if pos_index < len(indexed_dataset) else None
+
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data,
+        }
