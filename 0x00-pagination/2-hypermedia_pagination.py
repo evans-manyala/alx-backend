10b396d274e855a5a458256474b3cf95c009c70b
Evans Manyala <evans.manyala@gmail.com>
2024-07-21 22:46:40 +0300

    Error Fix Attempt

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,7 +6,7 @@ for pagination.
 """
 import csv
 import math
-from typing import Tuple, List, Dict
+from typing import Any, Tuple, List, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
