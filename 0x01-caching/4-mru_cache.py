e2e9b1d82e3c27c6a5de217608c4ce00df2b006e
Evans Manyala <evans.manyala@gmail.com>
2024-07-23 20:20:45 +0300

    Adding MRU Cache code logic

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+MRUCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class inherits from BaseCaching and implements MRU caching.
+    """
+
+    def __init__(self):
+        """
+        Initialize the MRUCache object.
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        Assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+            self.cache_data[key] = item
+            self.order.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discarded_key = self.order.pop(-2)
+                del self.cache_data[discarded_key]
+                print("DISCARD:", discarded_key)
+
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data[key]
