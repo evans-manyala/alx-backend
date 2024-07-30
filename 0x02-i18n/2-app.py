ecd0b20bea433d659b0bd35a8ba0ec8956a7f585
Evans Manyala <evans.manyala@gmail.com>
2024-07-30 14:53:50 +0300

    Adding get_locale function with the babel.localeselector decorator

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel for language support.
+
+This app serves a single page with a title and header,
+supporting multiple languages.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configuration for Flask app."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match with supported languages.
+
+    Uses the request's accept languages to find the best match.
+
+    Returns:
+        str: The best match language.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route("/")
+def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: Rendered HTML content of the index page.
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
