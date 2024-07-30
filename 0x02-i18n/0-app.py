710bb1a584720a3b90818a73bcd434f5e98e0cf0
Evans Manyala <evans.manyala@gmail.com>
2024-07-30 13:49:48 +0300

    Fixing errors with template loading

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -17,7 +17,7 @@ def index():
     Returns:
         str: Rendered HTML content of the index page.
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
