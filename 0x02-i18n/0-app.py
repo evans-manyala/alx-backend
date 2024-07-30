afa3150ace36ac27603e29823fea1dac96dcaef9
Evans Manyala <evans.manyala@gmail.com>
2024-07-30 13:30:41 +0300

    Basic Flask app with a single route and a template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with a single route and a template.
+
+This app serves a single page with a title and header.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+    
+    Returns:
+        str: Rendered HTML content of the index page.
+    """
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
