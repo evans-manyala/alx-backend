7cb4ad124b6acecc023443aef270d97880ab5d6d
Evans Manyala <evans.manyala@gmail.com>
2024-07-30 13:50:52 +0300

    Fixing pycodestyling errors

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,15 +9,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     """
     Render the index page.
-    
+
     Returns:
         str: Rendered HTML content of the index page.
     """
-    return render_template('0-index.html')
+    return render_template("0-index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
