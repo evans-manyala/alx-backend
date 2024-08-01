ed68555c6589caa837fcbde15e449734ecbb3701
Evans Manyala <evans.manyala@gmail.com>
2024-08-01 17:17:37 +0300

    Adding missing html file and code logic to main app to fix errors

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -34,6 +34,9 @@ def get_locale():
     Returns:
         str: The best match language.
     """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
@@ -45,10 +48,9 @@ def index() -> str:
     Returns:
         str: Rendered HTML content of the index page.
     """
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return render_template(
+        "4-index.html", title=_("home_title"), header=_("home_header")
+    )
 
 
 if __name__ == "__main__":
