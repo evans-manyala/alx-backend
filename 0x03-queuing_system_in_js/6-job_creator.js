2f515869ade92ddfa0e9ab6bc92f16b87a140e99
evans-manyala <evans.manyala@gmail.com>
2024-08-29 01:44:09 +0300

    Adding script

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,17 +3,15 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 const jobData = {
-        phoneNumber: '+254729334556',
-        message: 'Hello, this is a notification service',
+        phoneNumber: 'string',
+        message: 'string',
 };
 
-const job = queue.create('push_notification_code', jobData)
-        .save((err) => {
-                if (err) {
-                        console.error('Error creating job:', err);
-                        return;
+const job = queue.create('push_notification_code', jobData).save((err) => {
+        if (!err) {
+                console.log('Notification job created: ${job.id}');
         }
-        console.log('Notification job created: ${job.id}');
+        
 });
 
 job.on('complete', () => {
