b31bf0aaca0ecc2eb11f7abea06c9a7b49547aa2
evans-manyala <evans.manyala@gmail.com>
2024-08-28 06:38:34 +0300

    Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,21 +1,21 @@
-import redis from 'redi'
+import redis from 'redis';
 
 const subscriber = redis.createClient();
 
-subscriber.on('Connect', () => {
-        console.log('Redis client connected to the server');
+subscriber.on('connect', () => {
+    console.log('Redis client connected to the server');
 });
 
-subscriber.on('Error', (err) => {
-        console.log('Redis client not connected tot he server: ${err.message}');
+subscriber.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err.message}`);
 });
 
-subscriber.subscribe('Holberton School Channel');
+subscriber.subscribe('holberton school channel');
 
-subscriber.on('Message', (channel, message) => {
-        console.log('Recieved message: ${message}');
-        if (message === 'KiLL_SERVER') {
-                subscriber.unsubscribe('Holberton School Channel');
-                subscriber.quit();
-        }
+subscriber.on('message', (channel, message) => {
+    console.log(`Received message: ${message}`);
+    if (message === 'KILL_SERVER') {
+        subscriber.unsubscribe('holberton school channel');
+        subscriber.quit();
+    }
 });
